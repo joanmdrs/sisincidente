c4980452ffda1aec5e55a486e6c81d74eef6a09d
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-10-28 16:43:45 -0300

    feat: estruturando o component principal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import IncidentList from "./components/IncidentList";
+import IncidentForm from "./components/IncidentForm";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/NavBar";
+import Header from "./components/Header";
+import "./App.css"; // adicionaremos o CSS aqui
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="app-layout d-flex">
+          {/* Sidebar */}
+          <Navbar />
+
+          {/* Área principal */}
+          <div className="main-content flex-grow-1">
+            <Header />
+
+            <div className="content p-4">
+              <Routes>
+                {/* <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Navigate to="/dashboard" />} />
+                <Route
+                  path="/dashboard"
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                /> */}
+                <Route path="/incidents" element={<IncidentList />} />
+                <Route path="/incidents/:id" element={<IncidentForm />} />
+                <Route path="/new" element={<IncidentForm />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
